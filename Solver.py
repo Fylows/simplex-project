86196b6f1df3583c8bf84e60bb2ef8dbc0ffaf5a
fyyy <saplan.ljiel.dc@gmail.com>
2025-11-30 19:28:01 +0800

    test

--- Solver.py
+++ Solver.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import numpy as np
+import base64
 
 from Functions.projectsTable import ProjectsList
 import Functions.tableauMaker as tableauMaker
